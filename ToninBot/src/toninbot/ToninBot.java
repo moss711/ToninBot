064035fd432bdb517c0358eb638fa2db6069ab59
Alex <moss711@gmail.com>
2016-06-12 14:51:59 +0200

    Twitteo desde listener

--- ToninBot/src/toninbot/ToninBot.java
+++ ToninBot/src/toninbot/ToninBot.java
@@ -6,7 +6,13 @@
 
 package toninbot;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import twitter4j.FilterQuery;
+import twitter4j.StatusUpdate;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
 import twitter4j.TwitterStream;
 import twitter4j.TwitterStreamFactory;
 import twitter4j.auth.AccessToken;
@@ -28,16 +34,18 @@ public class ToninBot {
         ConfigurationBuilder builder = new ConfigurationBuilder();
         builder.setOAuthConsumerKey(Credenciales.consumerKey);
         builder.setOAuthConsumerSecret(Credenciales.consumerSecret);
+        
         Configuration configuration = builder.build();
-        TwitterStreamFactory factory = new TwitterStreamFactory(configuration);
-        TwitterStream twitterStream=factory.getInstance();
+        TwitterStreamFactory twStreamFactory = new TwitterStreamFactory(configuration);
+        TwitterStream twitterStream=twStreamFactory.getInstance();
         twitterStream.setOAuthAccessToken(accessToken);
         
         ToninStatusListener listener = new ToninStatusListener();
         twitterStream.addListener(listener);
         
         FilterQuery filtre = new FilterQuery();
-        filtre.follow(2841336351L,57322631L);
+        //filtre.follow(184742273L,2841338087L);//Allegue y proyectoPSIa1
+        filtre.follow(2841338087L);//proyectoPSIa1
         
         
         twitterStream.filter(filtre);
--- ToninBot/src/toninbot/ToninStatusListener.java
+++ ToninBot/src/toninbot/ToninStatusListener.java
@@ -6,46 +6,96 @@
 
 package toninbot;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import twitter4j.StallWarning;
 import twitter4j.Status;
 import twitter4j.StatusDeletionNotice;
 import twitter4j.StatusListener;
+import twitter4j.StatusUpdate;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.auth.AccessToken;
+import twitter4j.conf.Configuration;
+import twitter4j.conf.ConfigurationBuilder;
 
 /**
  *
  * @author Alex
  */
 public class ToninStatusListener implements StatusListener{
+    private Twitter twitter;
+
+    public ToninStatusListener() {
+        ConfigurationBuilder builder = new ConfigurationBuilder();
+        builder.setOAuthConsumerKey(Credenciales.consumerKey);
+        builder.setOAuthConsumerSecret(Credenciales.consumerSecret);
+        
+        Configuration configuration = builder.build();
+        AccessToken accessToken = new AccessToken(Credenciales.token, Credenciales.tokenSecret);
+        
+        TwitterFactory twFactory = new TwitterFactory(configuration);
+        twitter = twFactory.getInstance();
+        twitter.setOAuthAccessToken(accessToken);
+    }
+    
+    
 
     @Override
     public void onStatus(Status arg0) {
+        if (arg0.getUser().getId()!=184742273L && arg0.getUser().getId()!=2841338087L){
+            return;
+        }
         System.out.println(arg0.getUser().getName()+" "+arg0.getText());
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(arg0.getCreatedAt());
+        long hora=cal.get(Calendar.HOUR_OF_DAY);
+        System.out.println("Hora: "+cal.get(Calendar.HOUR_OF_DAY));
+        
+        if (hora>7){
+            return;
+        }
+        
+        String texto="Fora de horario!";
+        StatusUpdate stat= new StatusUpdate("@"+arg0.getUser().getScreenName()+" "+texto);
+        System.out.println("Fora de horario!");
+
+        stat.inReplyToStatusId(arg0.getId());
+
+        try {
+            twitter.updateStatus(stat);
+            System.out.println("Twitteado: "+stat.toString());
+        } catch (TwitterException ex) {
+            System.out.println("Error");
+        }
     }
 
     @Override
     public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public void onTrackLimitationNotice(int numberOfLimitedStatuses) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public void onScrubGeo(long userId, long upToStatusId) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public void onStallWarning(StallWarning warning) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public void onException(Exception excptn) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
 }
